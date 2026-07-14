bc763c1aaa7480f256098ce619108a011f2658ea
Jian Ai <23275590+jianai1220@users.noreply.github.com>
2026-07-14 16:21:03 +0800

    contact form API call updated
    the form works now

--- src/main.js
+++ src/main.js
@@ -39,3 +39,41 @@ window.addEventListener('beforeunload', () => {
   skillsScene?.dispose();
   projectGallery?.dispose();
 });
+
+// Contact Form - web3forms API Call
+const form = document.getElementById('contactForm');
+const submitBtn = form.querySelector('button[type="submit');
+
+form.addEventListener('submit', async(e)=>{
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  formData.append("access_key", "3f601a47-7d58-4494-92f0-7e1e8ace37c4");
+  
+  const originalText = submitBtn.textContent;
+
+  submitBtn.textContent = "Sending...";
+  submitBtn.disabled = true;
+
+  try{
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method:"POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if(response.ok){
+      alert("Success! Your message has been sent.");
+      form.reset();
+    }
+    else{
+      alert("Error: " + data.message);
+    }
+  } catch(error){
+    alert("Something went wrong. Please try again.")
+  }finally{
+    submitBtn.textContent = originalText;
+    submitBtn.disabled = false;
+  }
+});
